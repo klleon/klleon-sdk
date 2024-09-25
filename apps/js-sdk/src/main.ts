d3b9b0cc8d4e0aae871cab7cf958ebe9964c41ed
inhyuck-kim <inhyuck.kim@klleon.io>
2024-09-25 13:20:31 +0900

    fix

--- apps/js-sdk/src/main.ts
+++ apps/js-sdk/src/main.ts
@@ -1,4 +1,4 @@
-import sdkHandler from "./core/sdk"
+import { sdkHandler } from "./core/sdk"
 
 await sdkHandler.init({ sdk_key: 'app-1234567890', avatar_id: 'a5fe629d-0090-11ef-8ee1-0abbf354c5cc', locale: 'ko_kr' })
 
